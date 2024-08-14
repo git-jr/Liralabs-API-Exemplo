3247a38a74bffbd5d19f0f1ef4152c97e736bdb7
Junior <35709152+git-jr@users.noreply.github.com>
2024-08-14 15:48:34 -0300

    build: add libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,8 @@
 
 val kotlin_version: String by project
 val logback_version: String by project
+val h2_version: String by project
+val exposed_version: String by project
 
 plugins {
     kotlin("jvm") version "2.0.10"
@@ -29,6 +31,13 @@ dependencies {
     implementation("io.ktor:ktor-server-netty-jvm")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-server-config-yaml")
+
+
+    implementation("com.h2database:h2:$h2_version")
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+
     testImplementation("io.ktor:ktor-server-test-host-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
